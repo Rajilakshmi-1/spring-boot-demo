ed8e7b459e570d4381a962fe0654f16e4d345aa0
Rajyalakshmi@123 <peddireddi.rajya-lakshmi-satya-durga@capgemini.com>
2025-08-31 20:14:50 +0530

    Initial springboot boiler plate

---
+++ src/main/java/com/example/demov/DemovcApplication.java
@@ -0,0 +1,13 @@
+package com.example.demov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemovcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemovcApplication.class, args);
+	}
+
+}
